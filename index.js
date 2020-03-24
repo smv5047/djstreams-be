6c04c2f376e5fb9e5ee6ad0bc6ee013743778e79
smv5047 <smv5047@gmail.com>
2020-03-24 16:42:31 -0400

    updated index file

---
+++ index.js
@@ -0,0 +1,23 @@
+const dotenv = require("dotenv").config()
+const express = require("express")
+
+//Create a server instance
+const server = express()
+
+//Routers
+const EventsRouter = require("./events/events-router")
+
+//Middleware
+server.use(express.json())
+server.use("/api/events", EventsRouter)
+
+const PORT = process.env.PORT || 3333
+const HOST = process.env.HOST
+
+server.get("/", (req, res) => {
+    res.send("DJ Streams coming to you from the 1s & 2s")
+})
+
+server.listen(PORT, () => {
+    console.log(`Listening on port${PORT}`)
+})
