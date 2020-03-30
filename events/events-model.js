1721bc389ecc74ec4df5001b488c9997771641b6
smv5047 <smv5047@gmail.com>
2020-03-30 15:57:32 -0400

    updated add model

--- events/events-model.js
+++ events/events-model.js
@@ -14,9 +14,15 @@ function findById(id) {
 //CREATE
 
 const add = event => {
-    db("events")
-        .insert(event)
-        .returning("*")
+    // db("events")
+    //     .insert(event)
+    //     .returning("*")
+
+        const [id] = await db("events").insert(event)	    
+        console.log(id)
+        return db("event")
+            .where({ id })
+            .returning("*")
 }
 
 //UPDATE
