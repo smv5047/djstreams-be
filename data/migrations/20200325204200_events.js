f56a21141560d7f0ec41183bbd5520d3cda635e9
smv5047 <smv5047@gmail.com>
2020-04-19 16:32:51 -0400

    updated tables to add artist to events

--- data/migrations/20200325204200_events.js
+++ data/migrations/20200325204200_events.js
@@ -1,9 +1,6 @@
-exports.up = async function(knex) {
-    await knex.schema.createTable("events", table => {
-        table
-            .increments("id")
-            .notNullable()
-            .unique()
+exports.up = async function (knex) {
+    await knex.schema.createTable("events", (table) => {
+        table.increments("id").notNullable().unique()
         table.string("event_name").notNullable()
         table.string("event_start")
         table.string("event_end")
@@ -16,20 +13,16 @@ exports.up = async function(knex) {
         table.string("link").notNullable()
         table.string("genre")
         table.boolean("isPromoted")
+        //Remove in future release and use additional tables
+        table.string("artist")
     })
 
-    knex.schema.createTable("artists", table => {
-        table
-            .increments("id")
-            .notNullable()
-            .unique()
-        table
-            .string("name")
-            .notNullable()
-            .unique()
+    knex.schema.createTable("artists", (table) => {
+        table.increments("id").notNullable().unique()
+        table.string("name").notNullable().unique()
     })
 
-    knex.schema.createTable("event_artists", table => {
+    knex.schema.createTable("event_artists", (table) => {
         table
             .integer("event_id")
             .notNullable()
@@ -48,7 +41,7 @@ exports.up = async function(knex) {
     })
 }
 
-exports.down = async function(knex) {
+exports.down = async function (knex) {
     await knex.schema.dropTableIfExists("event_artists")
     await knex.schema.dropTableIfExists("artists")
     await knex.schema.dropTableIfExists("events")
